b625e57a306ecb21f5881969ce57bd68c6ebefb0
TRAVIS SU <Scarittagle@users.noreply.github.com>
2017-10-01 18:24:37 -0400

    Changed search method using os.walk()
    With old method for comparison

--- HW4-Bigfile.py
+++ HW4-Bigfile.py
@@ -19,21 +19,33 @@ def bigfiles():
     big = []
 
     #Search
-    for item in os.listdir(basepth):
-        item = basepth + "/" + item
-        #make recursive if path is exist and is a directory (assume the basepth folder have subfolders inside)
-        if os.path.isdir(item):
-            bigfiles(item)
-        else:
+    os.chdir(basepth)
+    for root, dirs, files in os.walk(basepth, topdown=True):
+        for name in files:
+            item = os.path.join(root, name)
             itemsize = os.path.getsize(item)
-            #find file size more than 100MB
-            if itemsize > 100000000:
+            if itemsize > 10000:
                 big.append((item, itemsize))
 
+    #for item in os.listdir(basepth):
+        #item = basepth + "/" + item
+        #make recursive if path is exist and is a directory (assume the basepth folder have subfolders inside)
+        #if os.path.isdir(item):
+        #    bigfiles(item)
+        #else:
+        #    itemsize = os.path.getsize(item)
+            #find file size more than 100MB
+        #    if itemsize > 100000000:
+        #        big.append((item, itemsize))
+
     #Found! Delete it
-    if big is not None:
+    if big:
         for item in big:
             print ("File found, PATH: %s" %item[0])
             print ("deleting....")
             os.remove(item[0])
             print ("File Deleted.")
+    else:
+        print("This folder and it's subfolder(s) does not contain any files exceeded 100MB.")
+
+bigfiles()
